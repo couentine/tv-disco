829a7ced000cfdc6e686b30d3dcdb4f1f41ae8f3
couentine <quent.guillaume@gmail.com>
2019-05-07 11:34:25 -0700

    Fixing typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,9 @@
 class ApplicationController < ActionController::Base
 
   include HTTParty #removed to test TVController view rendering issue
-  
-  require 'addressable/uri'
-  require 'addressable/template'
 
-    def index
-        super
-    end
+  def index
+   super
+  end
 
 end
